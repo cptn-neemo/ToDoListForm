9e7b8d47383e30171dbb80b2d44b1439e5011862
Max <maxhuddleston@gmail.com>
2017-05-21 22:39:05 -0500

    ToDoListForm

---
+++ ToDoListForm/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ToDoListForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.descBox = new System.Windows.Forms.TextBox();
+            this.deleteTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(673, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "To Do List App";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create Item";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(290, 286);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(175, 20);
+            this.titleBox.TabIndex = 2;
+            this.titleBox.Text = "Title";
+            // 
+            // descBox
+            // 
+            this.descBox.Location = new System.Drawing.Point(290, 312);
+            this.descBox.Name = "descBox";
+            this.descBox.Size = new System.Drawing.Size(175, 20);
+            this.descBox.TabIndex = 3;
+            this.descBox.Text = "Description";
+            // 
+            // deleteTask
+            // 
+            this.deleteTask.Location = new System.Drawing.Point(290, 389);
+            this.deleteTask.Name = "deleteTask";
+            this.deleteTask.Size = new System.Drawing.Size(75, 23);
+            this.deleteTask.TabIndex = 4;
+            this.deleteTask.Text = "Delete Task";
+            this.deleteTask.UseVisualStyleBackColor = true;
+            this.deleteTask.Click += new System.EventHandler(this.deleteTask_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 535);
+            this.Controls.Add(this.deleteTask);
+            this.Controls.Add(this.descBox);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox descBox;
+        private System.Windows.Forms.Button deleteTask;
+    }
+}
+
---
+++ ToDoListForm/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ToDoListForm
+{
+    public partial class Form1 : Form
+    {
+        List<CheckBox> rList = new List<CheckBox>();
+        int num = 0;
+        int high = 30;
+        string path = @"C:\Users\default.bigamus\Documents\Visual Studio 2017\Projects\Challenges\ToDoListForm";
+
+        public Form1()
+        {
+            InitializeComponent();
+            File.Create(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (high == 510)
+            {
+                MessageBox.Show("Too many items on the list.", "Warning: ", MessageBoxButtons.OK);
+            }
+            else
+            {
+
+                rList.Add(new CheckBox());
+                rList[num].Location = new Point(30, high);
+                this.Controls.Add(rList[num]);
+
+                string totalText = titleBox.Text + ": ";
+                totalText += descBox.Text;
+
+                rList[num].AutoSize = true;
+                rList[num].Text = totalText;
+
+                titleBox.Text = "Title";
+                descBox.Text = "Description";
+
+                high += 20;
+                num++;
+            }
+        }
+
+        private void deleteTask_Click(object sender, EventArgs e)
+        {
+            int times = 0;
+            for (int i = 0; i < rList.Count; i++)
+            {
+                if (rList[i].Checked == true)
+                {
+                    this.Controls.Remove(rList[i]);
+                    rList.Remove(rList[i]);
+                    times++;
+                    i--;
+                }
+            }
+
+            high = 30;
+
+            foreach(CheckBox x in rList)
+            {
+                x.Location = new Point(30, high);
+                high += 20;
+            }
+
+            num -= times;
+        }
+    }
+}
